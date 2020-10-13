bf8af613e377d326a3b9d6d296ea9c78fa05fa9b
Papowe <63293783+Papowe@users.noreply.github.com>
2020-10-13 13:04:48 +0300

    Completed homework in lesson 2.

--- Lesson2/L2 - Solution5/Program.cs
+++ Lesson2/L2 - Solution5/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace L2___Solution5
@@ -10,6 +11,35 @@ namespace L2___Solution5
     {
         static void Main(string[] args)
         {
+            // а) Написать программу, которая запрашивает массу и рост человека, 
+            // вычисляет его индекс массы и сообщает, нужно ли человеку похудеть, набрать вес или всё в норме.
+            // б) *Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса.
+
+            double h, m, i;
+
+            Console.Write("Ваш Рост: ");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ваш Вес: ");
+            m = Convert.ToDouble(Console.ReadLine());
+
+            i = m / (Math.Pow(h, 2));
+
+            Console.WriteLine($"Ваш индек массы тела: {i:F2}");
+
+            if (i > 0 && i <= 18.5)
+            {
+                Console.WriteLine("Недостаточная (дефицит) масса тела, вам нужно поправиться");
+            }
+            else if (i > 18.5 && i <= 25)
+            {
+                Console.WriteLine("У вас нормальный вес");
+            }
+            else if (i > 25)
+            {
+                Console.WriteLine("Избыточная масса тела, вам нужно похудеть");
+            }
+            
         }
     }
 }
