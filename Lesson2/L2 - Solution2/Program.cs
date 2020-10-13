e1ff9975259ddfcdfae13ba5a01675a11973f6aa
Papowe <63293783+Papowe@users.noreply.github.com>
2020-10-13 11:10:01 +0300

    Completed the second task.

--- Lesson2/L2 - Solution2/Program.cs
+++ Lesson2/L2 - Solution2/Program.cs
@@ -8,9 +8,22 @@ namespace L2___Solution2
 {
     class Program
     {
+        // Написать метод подсчета количества цифр числа.
+
+
         static void Main(string[] args)
         {
+            CoutnNumbers(522);
+        }
 
+        static void CoutnNumbers(int a)
+        {
+            int i;
+            for (i = 0; a!= 0; i++)
+            {
+                a = a / 10;
+            }
+            Console.WriteLine("Количиство чисел = " + i);
         }
     }
 }
