fdf9d553a03c1c503ef2072827ff7b4577c0f67f
Papowe <63293783+Papowe@users.noreply.github.com>
2020-10-17 15:12:11 +0300

    Completed the third task of the third lesson

--- Lesson3/L3 - Solution3/Program.cs
+++ Lesson3/L3 - Solution3/Program.cs
@@ -10,6 +10,30 @@ namespace L3___Solution3
     {
         static void Main(string[] args)
         {
+            // * Описать класс дробей -рациональных чисел, являющихся отношением двух целых чисел.
+            // Предусмотреть методы сложения, вычитания, умножения и деления дробей. 
+            // Написать программу, демонстрирующую все разработанные элементы класса. Достаточно решить 2 задачи.Все программы сделать в одном решении.
+
+            // **Добавить проверку, чтобы знаменатель не равнялся 0.Выбрасывать исключение
+            // ArgumentException("Знаменатель не может быть равен 0");
+            // Добавить упрощение дробей.
+
+            RationalNumber a = new RationalNumber(4, 3);
+            RationalNumber b = new RationalNumber(1, 2);
+
+            RationalNumber rezAdding = RationalNumber.Adding(a, b);
+            Console.WriteLine(rezAdding.ToString());
+
+            RationalNumber rezSubtraction = RationalNumber.Subtraction(a, b);
+            Console.WriteLine(rezSubtraction.ToString());
+
+            RationalNumber rezMultiplication = RationalNumber.Multiplication(a, b);
+            Console.WriteLine(rezMultiplication.ToString());
+
+            RationalNumber rezDivision = RationalNumber.Division(a, b);
+            Console.WriteLine(rezDivision.ToString());
+
+
         }
     }
 }
---
+++ Lesson3/L3 - Solution3/RationalNumber.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L3___Solution3
+{
+    class RationalNumber
+    {
+        private int numerator;
+        private int denominator;
+
+        public RationalNumber(int numerator, int denominator)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+            if (denominator == 0)
+            {
+                throw new ArgumentException("Знаменатель не может быть равен 0");
+            }
+        }
+        public RationalNumber()
+        {
+        }
+
+        public static RationalNumber Adding(RationalNumber firstRationalNumber, RationalNumber secondRationalNumber)
+        {
+            RationalNumber r = new RationalNumber();
+            r.numerator = (firstRationalNumber.numerator * secondRationalNumber.denominator) + (firstRationalNumber.denominator * secondRationalNumber.numerator);
+            r.denominator = firstRationalNumber.denominator * secondRationalNumber.denominator;
+            return r;
+        }        
+        
+        public static RationalNumber Subtraction(RationalNumber firstRationalNumber, RationalNumber secondRationalNumber)
+        {
+            RationalNumber r = new RationalNumber();
+            r.numerator = (firstRationalNumber.numerator * secondRationalNumber.denominator) - (firstRationalNumber.denominator * secondRationalNumber.numerator);
+            r.denominator = firstRationalNumber.denominator * secondRationalNumber.denominator;
+            return r;
+        }
+
+        public static RationalNumber Multiplication(RationalNumber firstRationalNumber, RationalNumber secondRationalNumber)
+        {
+            RationalNumber r = new RationalNumber();
+            r.numerator = firstRationalNumber.numerator * secondRationalNumber.numerator;
+            r.denominator = firstRationalNumber.denominator * secondRationalNumber.denominator;
+            return r;
+        }
+
+        public static RationalNumber Division(RationalNumber firstRationalNumber, RationalNumber secondRationalNumber)
+        {
+            RationalNumber r = new RationalNumber();
+            r.numerator = firstRationalNumber.numerator * secondRationalNumber.denominator;
+            r.denominator = firstRationalNumber.denominator * secondRationalNumber.numerator;
+            return r;
+        }
+
+        public override string ToString()
+        {
+            return $"n{numerator} d{denominator}";
+        }
+    }
+}
