dccb9e8cab03b93d430bc990425b395581612cf8
Papowe <63293783+Papowe@users.noreply.github.com>
2020-10-17 10:25:02 +0300

    Completed the first task of the third lesson.

---
+++ Lesson3/L3 - Solution1/ClassComplex.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L3___Solution1
+{
+    class ClassComplex
+    {
+        public double im;
+        public double re;
+
+        public ClassComplex Plus(ClassComplex x)
+        {
+            ClassComplex y = new ClassComplex();
+            y.im = im + x.im;
+            y.re = re + x.re;
+            return y;
+        }
+
+        public ClassComplex Minus(ClassComplex x)
+        {
+            ClassComplex y = new ClassComplex();
+            y.im = im - x.im;
+            y.re = re - x.re;
+            return y;
+        }
+
+        public ClassComplex Milti(ClassComplex x)
+        {
+            ClassComplex y = new ClassComplex();
+            y.im = re * x.im + im * x.re;
+            y.re = re * x.re - im * x.im;
+            return y;
+        }
+        public string ToString()
+        {
+            return re + "+" + im + "i";
+        }
+    }
+}
---
+++ Lesson3/L3 - Solution1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L3___Solution1
+{
+    class Program
+    {
+        // а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.Продемонстрировать работу структуры;
+        // б) Дописать класс Complex, добавив методы вычитания и произведения чисел.Проверить работу класса;
+
+        static void Main(string[] args)
+        {
+            //----------------------ПРИМЕР КОМПЛЕКСНЫХ ЧИСЕЛ СТРУКТУР------------------
+            StructComplex complex1;
+            complex1.im = 34;
+            complex1.re = 6;
+
+            StructComplex complex2;
+            complex2.im = 3;
+            complex2.re = 2;
+
+            StructComplex rezultComplex = complex1.Minus(complex2);
+            Console.WriteLine(rezultComplex.ToString());
+
+            //---------------------ПРИМЕР КОМПЛЕКСНЫХ ЧИСЕЛ КЛАССОВ-------------------
+            ClassComplex complex3 = new ClassComplex();
+            complex3.im = 10;
+            complex3.re = 8;
+
+            ClassComplex complex4 = new ClassComplex();
+            complex3.im = 1;
+            complex3.re = 4;
+
+            ClassComplex rezultClass = complex3.Minus(complex4);
+            Console.WriteLine(rezultClass.ToString());
+
+
+
+
+        }
+    }
+}
