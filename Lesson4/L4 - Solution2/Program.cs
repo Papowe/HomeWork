6a5048e3e22af73aacbc3f8db2369371655a75b6
Papowe <63293783+Papowe@users.noreply.github.com>
2020-10-20 15:43:59 +0300

    Completed the second task of the fourth lesson.

--- Lesson4/L4 - Solution2/Program.cs
+++ Lesson4/L4 - Solution2/Program.cs
@@ -10,6 +10,25 @@ namespace L4___Solution2
     {
         static void Main(string[] args)
         {
+            // Реализуйте задачу 1 в виде статического класса StaticClass;
+            // а) Класс должен содержать статический метод, который принимает на вход массив и решает задачу 1;
+            // б) *Добавьте статический метод для считывания массива из текстового файла. Метод должен возвращать массив целых чисел;
+            // в)**Добавьте обработку ситуации отсутствия файла на диске.
+
+            int[] arr = new int[30];
+
+            StaticClassArray.ArrayPlaceholder(arr);
+            StaticClassArray.SearchCouples(arr);
+
+            Console.WriteLine();
+
+            int[] s;
+            s = StaticClassArray.ReadFromFile("file.txt");
+            foreach (var item in s)
+            {
+                Console.WriteLine(item);               
+            }
+
         }
     }
 }
---
+++ Lesson4/L4 - Solution2/StaticClassArray.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4___Solution2
+{
+    static class StaticClassArray
+    {
+        /// <summary>
+        /// Метод для заполнения массива случайными числами от –10 000 до 10 000 включительно.
+        /// </summary>
+        /// <param name="a">Массив</param>
+        static public void ArrayPlaceholder(int[] a)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rnd.Next(-10_000, 10_001);
+            }
+        }
+        /// <summary>
+        /// Выводит количество пар элементов массива, в которых только одно число делится на 3.
+        /// </summary>
+        /// <param name="a">Массив</param>
+        static public void SearchCouples(int[] a)
+        {
+            int count = 0;
+
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+
+                if (a[i] % 3 == 0 || a[i + 1] % 3 == 0)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Ответ = {count}");
+        }
+
+        /// <summary>
+        /// Считывает массив из текстового файла. Метод возвращает массив целых чисел;
+        /// </summary>
+        /// <param name="path">Путь к файлу</param>
+        /// <returns></returns>
+        static public int[] ReadFromFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                string[] ss = File.ReadAllLines(path);
+                int[] tempIntarray = new int[ss.Length];
+
+                for (int i = 0; i < ss.Length; i++)
+                {
+                    tempIntarray[i] = int.Parse(ss[i]);
+                }
+
+                return tempIntarray;
+            }
+            else
+            {
+                Console.WriteLine("Error load file");
+                return new int[1] { 0 }; ;
+            }
+        }
+    }
+}
