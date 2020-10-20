dc25f469f305b5d360578994fa75a093e24222b6
Papowe <63293783+Papowe@users.noreply.github.com>
2020-10-20 21:28:57 +0300

    продолжение завтра.

---
+++ Lesson4/L4 - Solution3/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4___Solution3
+{
+    class MyClass
+    {
+        public int[] arr;
+
+        public MyClass(int elements, int initial, int step)
+        {
+            arr = new int[elements];
+            arr[0] = initial;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                arr[i] = arr[i - 1] + step;
+            }
+        }
+    }
+}
