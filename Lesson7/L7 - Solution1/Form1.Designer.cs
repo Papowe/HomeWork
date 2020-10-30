c6230eb5e9cfc8bccb67b86fb0f9dad9d57395c5
Papowe <63293783+Papowe@users.noreply.github.com>
2020-10-30 21:30:49 +0200

    Completed the first task of the seven lesson.

---
+++ Lesson7/L7 - Solution1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace L7___Solution1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCommand1 = new System.Windows.Forms.Button();
+            this.btnCommand2 = new System.Windows.Forms.Button();
+            this.btnCommand3 = new System.Windows.Forms.Button();
+            this.lblNumbers = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MenuPlay = new System.Windows.Forms.ToolStripMenuItem();
+            this.cancel = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCommand1
+            // 
+            this.btnCommand1.Location = new System.Drawing.Point(449, 95);
+            this.btnCommand1.Name = "btnCommand1";
+            this.btnCommand1.Size = new System.Drawing.Size(95, 23);
+            this.btnCommand1.TabIndex = 0;
+            this.btnCommand1.Text = "+1";
+            this.btnCommand1.UseVisualStyleBackColor = true;
+            this.btnCommand1.Click += new System.EventHandler(this.btnCommand1_Click);
+            // 
+            // btnCommand2
+            // 
+            this.btnCommand2.Location = new System.Drawing.Point(449, 124);
+            this.btnCommand2.Name = "btnCommand2";
+            this.btnCommand2.Size = new System.Drawing.Size(95, 23);
+            this.btnCommand2.TabIndex = 1;
+            this.btnCommand2.Text = "x2";
+            this.btnCommand2.UseVisualStyleBackColor = true;
+            this.btnCommand2.Click += new System.EventHandler(this.btnCommand2_Click);
+            // 
+            // btnCommand3
+            // 
+            this.btnCommand3.Location = new System.Drawing.Point(449, 153);
+            this.btnCommand3.Name = "btnCommand3";
+            this.btnCommand3.Size = new System.Drawing.Size(95, 23);
+            this.btnCommand3.TabIndex = 2;
+            this.btnCommand3.Text = "Сброс";
+            this.btnCommand3.UseVisualStyleBackColor = true;
+            this.btnCommand3.Click += new System.EventHandler(this.btnCommand3_Click);
+            // 
+            // lblNumbers
+            // 
+            this.lblNumbers.AutoSize = true;
+            this.lblNumbers.Location = new System.Drawing.Point(157, 124);
+            this.lblNumbers.Name = "lblNumbers";
+            this.lblNumbers.Size = new System.Drawing.Size(16, 16);
+            this.lblNumbers.TabIndex = 3;
+            this.lblNumbers.Text = "0";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(12, 269);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(248, 16);
+            this.lblCount.TabIndex = 4;
+            this.lblCount.Text = "Количества отданных команд : 0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuPlay});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(672, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MenuPlay
+            // 
+            this.MenuPlay.Name = "MenuPlay";
+            this.MenuPlay.Size = new System.Drawing.Size(55, 20);
+            this.MenuPlay.Text = "Играть";
+            this.MenuPlay.Click += new System.EventHandler(this.MenuPlay_Click);
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(449, 182);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(95, 23);
+            this.cancel.TabIndex = 6;
+            this.cancel.Text = "Отменить";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(672, 294);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblNumbers);
+            this.Controls.Add(this.btnCommand3);
+            this.Controls.Add(this.btnCommand2);
+            this.Controls.Add(this.btnCommand1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCommand1;
+        private System.Windows.Forms.Button btnCommand2;
+        private System.Windows.Forms.Button btnCommand3;
+        private System.Windows.Forms.Label lblNumbers;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MenuPlay;
+        private System.Windows.Forms.Button cancel;
+    }
+}
+
---
+++ Lesson7/L7 - Solution1/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L7___Solution1
+{
+    public partial class Form1 : Form
+    {
+        // а) Добавить в программу «Удвоитель» подсчёт количества отданных команд удвоителю.
+        // б) Добавить меню и команду «Играть». При нажатии появляется сообщение, какое число должен получить игрок.
+        // Игрок должен получить это число за минимальное количество ходов.
+        // в) *Добавить кнопку «Отменить», которая отменяет последние ходы.Используйте обобщенный класс Stack.
+        // Вся логика игры должна быть реализована в классе с удвоителем.
+        
+        /// <summary>
+        /// Подсчет шагов
+        /// </summary>
+        int countCommand = 0;
+        /// <summary>
+        /// Стек для хранения истории чисел
+        /// </summary>
+        Stack<string> history = new Stack<string>();
+
+        public Form1()
+        {
+            InitializeComponent();           
+        }
+
+        /// <summary>
+        /// Кнопка добавляет 1 к числам и записывает в стек.
+        /// </summary>
+        private void btnCommand1_Click(object sender, EventArgs e)
+        {
+            lblNumbers.Text = (int.Parse(lblNumbers.Text) + 1).ToString();
+            Counter(1);
+            AddToHistory();
+        }
+
+        /// <summary>
+        /// Кнопка умножает на 2 числа и записывает в стек.
+        /// </summary>
+        private void btnCommand2_Click(object sender, EventArgs e)
+        {
+            lblNumbers.Text = (int.Parse(lblNumbers.Text) * 2).ToString();
+            Counter(1);
+            AddToHistory();
+        }
+        /// <summary>
+        /// Кнопка все обнуляет
+        /// </summary>
+        private void btnCommand3_Click(object sender, EventArgs e)
+        {
+            lblNumbers.Text = "1";
+            lblCount.Text = "Количества отданных команд : 0";
+            countCommand = 0;
+
+            for (int i = 0; i < history.Count; i++)
+            {
+                history.Pop();
+            }
+        }
+       
+        /// <summary>
+        /// Кнопка отменяет последние действия
+        /// </summary>
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            Counter(-1);
+            Cancel();
+        }
+        /// <summary>
+        /// Кнопка рандомно выберает сколько нужно набрать чилел и за сколько попыток
+        /// </summary>
+        private void MenuPlay_Click(object sender, EventArgs e)
+        {
+            Random rndNumer = new Random();
+            Random rndAttempt = new Random();
+            int attempt = rndAttempt.Next(5, 11);
+            int number = rndNumer.Next(101);
+
+            MessageBox.Show($"Наберите число : {number} за {attempt} попыток.");
+        }
+        /// <summary>
+        /// Метод изменяет количичтво команд
+        /// </summary>
+        /// <param name="a">Можна добавить число к количиству команд, или отнять</param>
+        private void Counter(int a)
+        {
+            countCommand += a;
+            if (countCommand < 0)
+            {
+                countCommand = 0;
+            }
+            lblCount.Text = $"Количества отданных команд : {countCommand}";
+        }
+
+        /// <summary>
+        /// Метод записывает номера в стек
+        /// </summary>
+        private void AddToHistory()
+        {
+            history.Push(lblNumbers.Text);            
+        }
+
+        /// <summary>
+        /// Метод отменяет действия
+        /// </summary>
+        private void Cancel()
+        {
+            if (history.Count.Equals(0)) return;
+            lblNumbers.Text = history.Pop();              
+        }
+    }
+}
