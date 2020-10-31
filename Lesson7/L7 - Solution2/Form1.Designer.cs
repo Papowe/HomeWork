2f851f164ab0e38aca487a20f8a6d810b402491a
Papowe <63293783+Papowe@users.noreply.github.com>
2020-10-31 09:00:41 +0200

    Completed the second task of the seven lesson.

--- Lesson7/L7 - Solution2/Form1.Designer.cs
+++ Lesson7/L7 - Solution2/Form1.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.enter = new System.Windows.Forms.Button();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.lblTxtInfo = new System.Windows.Forms.Label();
+            this.lblInfoGame = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // enter
+            // 
+            this.enter.Location = new System.Drawing.Point(60, 90);
+            this.enter.Name = "enter";
+            this.enter.Size = new System.Drawing.Size(125, 47);
+            this.enter.TabIndex = 0;
+            this.enter.Text = " Проверить";
+            this.enter.UseVisualStyleBackColor = true;
+            this.enter.Click += new System.EventHandler(this.enter_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(259, 90);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(188, 26);
+            this.txtNumber.TabIndex = 1;
+            this.txtNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblTxtInfo
+            // 
+            this.lblTxtInfo.AutoSize = true;
+            this.lblTxtInfo.Location = new System.Drawing.Point(255, 119);
+            this.lblTxtInfo.Name = "lblTxtInfo";
+            this.lblTxtInfo.Size = new System.Drawing.Size(192, 20);
+            this.lblTxtInfo.TabIndex = 2;
+            this.lblTxtInfo.Text = "Вводить число сюда.";
+            // 
+            // lblInfoGame
+            // 
+            this.lblInfoGame.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lblInfoGame.AutoSize = true;
+            this.lblInfoGame.Location = new System.Drawing.Point(56, 240);
+            this.lblInfoGame.Name = "lblInfoGame";
+            this.lblInfoGame.Size = new System.Drawing.Size(0, 20);
+            this.lblInfoGame.TabIndex = 3;
+            this.lblInfoGame.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(13, 13);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(0, 20);
+            this.lblCount.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(530, 269);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblInfoGame);
+            this.Controls.Add(this.lblTxtInfo);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.enter);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button enter;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label lblTxtInfo;
+        private System.Windows.Forms.Label lblInfoGame;
+        private System.Windows.Forms.Label lblCount;
     }
 }
 
--- Lesson7/L7 - Solution2/Form1.cs
+++ Lesson7/L7 - Solution2/Form1.cs
@@ -10,11 +10,70 @@ using System.Windows.Forms;
 
 namespace L7___Solution2
 {
+        // Используя Windows Forms, разработать игру «Угадай число». Компьютер загадывает число от 1 до 100, 
+        // а человек пытается его угадать за минимальное число попыток.Компьютер говорит, больше или меньше загаданное число введенного.
+        // a) Для ввода данных от человека используется элемент TextBox;
+        // б) ** Реализовать отдельную форму c TextBox для ввода числа.
+
+
     public partial class Form1 : Form
     {
+        private int secretNumber;
+        private int count;
+        Random rnd = new Random();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Записывает рандомное число в переменную и выводит сообщение.
+            secretNumber = rnd.Next(1, 101);
+            MessageBox.Show($"Компьютер загадал число от 1 до 100. попробуйте его отгадать. {secretNumber}");
+        }
+
+        private void enter_Click(object sender, EventArgs e)
+        {
+            // Проверка на пустоту поля
+            if (txtNumber.Text != "")
+            {
+                bool isNumber = int.TryParse(txtNumber.Text, out int temp);
+                
+                //Проверка введено ли число
+                if (isNumber)
+                {
+                    Counter();
+
+                    if (temp < secretNumber) // Если число меньше выводит сообщение
+                    {
+                        lblInfoGame.Text = "Число меньше загаданного.";
+                    }
+                    else if (temp > secretNumber) // Если число больше выводит сообщение
+                    {
+                        lblInfoGame.Text = "Число больше загаданного.";
+                    }
+                    else if (temp == secretNumber) // Если число ровно выводит сообщение про победу и закрывает програму.
+                    {
+                        MessageBox.Show($"Вы победили. За {count} попыток.");
+                        Close();
+                    }
+                }
+                else
+                {
+                    lblInfoGame.Text = "Нужно вводить числа.";
+                }
+            }
+            
+        }
+
+        /// <summary>
+        /// Метод для подсчета попыток
+        /// </summary>
+        private void Counter()
+        {
+            count++;
+            lblCount.Text = $"Попыток : {count}";
         }
     }
 }
